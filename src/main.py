7d627d6099d0c1bb7355ac613d2f8158b882c45f
Seenetos <seenetoss@gmail.com>
2026-06-10 22:34:03 +0200

    Add sample weather response for development

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=52.3759&longitude=9.7320&current=temperature_2m,relative_humidity_2m,wind_speed_10m&timezone=Europe/Berlin")
                         
@@ -11,3 +12,7 @@ print("----------------")
 print(f"Temperature: {current['temperature_2m']} {current_units['temperature_2m']}")
 print(f"Humidity: {current['relative_humidity_2m']} {current_units['relative_humidity_2m']}")
 print(f"Wind Speed: {current['wind_speed_10m']} {current_units['wind_speed_10m']}")
+
+# Save a sample API response for development and testing
+#with open("data/samples/sample_weather.json", "w") as file:
+#    json.dump(data, file, indent=4)
